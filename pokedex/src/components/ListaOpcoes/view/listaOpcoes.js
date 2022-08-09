91c077edd9277f8c0969d4d70026c9decf5006ae
Guilherme Souza Araújo <guilhermesouzae21@gmail.com>
2022-08-08 21:28:21 -0300

    Name list

--- pokedex/src/components/ListaOpcoes/view/listaOpcoes.js
+++ pokedex/src/components/ListaOpcoes/view/listaOpcoes.js
@@ -1,28 +1,26 @@
 import { useEffect, useState } from 'react'
 import ListaOpcoesService from '../services/listaOpcoesService';
-// import { List, Grid } from '@mui/material';
+import { List, ListItem, ListItemText, Grid } from '@mui/material';
 
 function ListaOpcoes() {
 	const [listaPokemons, setListaPokemons] = useState([]);
 
 	useEffect(() => {
 		const getListagem = async () => {
-			let retorno = await ListaOpcoesService.obterListagem(100, 0);
+			let retorno = await ListaOpcoesService.obterListagem(5, 0);
 			setListaPokemons(retorno);
 		}
 		getListagem();
 	}, []);
-
-
+	console.log(listaPokemons);
 	return (
-		// <Grid container lg={12}>
-		// 	<Grid item lg={6}>
-		// 		<List>
-		// 			<p>Teste</p>
-		// 		</List>
-		// 	</Grid>
-		// </Grid>
-		<p>Lista</p>
+		<Grid container lg={12}>
+			<Grid item lg={6}>
+				<List dense={true}>
+					{listaPokemons.map((p) => <ListItemText primary={p.name} />)}
+				</List>
+			</Grid>
+		</Grid>
 	);
 }
 
---
+++ pokedex/src/muiConfig.js
@@ -0,0 +1,11 @@
+import { injectGlobal } from 'styled-components';
+const GlobalStyle = injectGlobal`
+html {
+    height: 100%
+}
+* {
+    padding: 0;
+    margin: 0
+}
+`
+export default GlobalStyle;
